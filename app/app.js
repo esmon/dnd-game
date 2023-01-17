6b17ddc260dec3d376d5a93b02791ad87159dc88
Simon <smonyk@gmail.com>
2023-01-17 04:32:03 -0500

    refactor(): app logic * replace request with superagent * update ui * using localstorage

--- app/app.js
+++ app/app.js
@@ -1,42 +1,33 @@
 new Vue({
   el: '#app',
   data: {
+    challenge_rating: 'challenge_rating=0&challenge_rating=0.125&challenge_rating=0.25',
     gameState: false,
     gameStats: {
       loses: 0,
       runAways: 0,
       wins: 0
     },
-    monsterAttackCompleted: true,
-    monsterAvatar: null,
-    monsterDice: '2d8',
-    monsterName: '',
-    monsterHealth: 100,
-    monsterMaxHealth: '',
-    playerAvatar: null,
-    playerHealthBar: 100,
-    playerHealthCount: '',
-    playerMaxHealth: '',
-    playerName: '',
-    playerDice: '',
-    playerWeaponName: '',
+    monster: {},
+    player: {},
     turns: []
   },
   beforeMount () {
     this.startGame();
   },
   methods: {
-    attack: function() {
-      if (this.monsterAttackCompleted) {
-        const damage = this.calcDamage(this.playerDice);
+    attack: function(dice, weaponName) {
+      if (this.monster.isAlive && this.monster.attackCompleted) {
+        const damage = this.calcDamage(dice);
 
-        this.monsterHealth -= Math.floor((damage/this.monsterMaxHealth) * 100);
-        this.monsterMaxHealth -= damage;
-        this.logEvent(true, this.playerName, this.monsterName, damage);
+        this.monster.healthBar -= Math.floor((damage/this.monster.health) * 100);
+        this.monster.health -= damage;
+        this.monster.attackCompleted = false;
 
-        if (this.checkWin()) return;
+        if (this.monster.health <= 0) this.monster.isAlive = false;
 
-        this.monsterAttack();
+        this.logEvent(true, damage, weaponName);
+        this.checkWin();
       }
     },
 
@@ -49,6 +40,7 @@ new Vue({
       if (numberOfDice > 1) {
         const die1 = randomDmgCalc;
         const die2 = randomDmgCalc;
+
         return die1 + die2;
       }
 
@@ -58,80 +50,142 @@ new Vue({
     checkWin: function() {
       const self = this;
 
-      if (this.monsterMaxHealth && this.monsterHealth <= 0) {
-        this.monsterMaxHealth = 0;
-        this.monsterHealth = 0;
+      if (this.monster.isAlive && !this.monster.attackCompleted) {
+        this.monsterAttack();
+      } else if (!this.monster.isAlive) {
+        this.gameState = false;
+        this.monster.health = 0;
+        this.monster.healthBar = 0;
+        this.monster.attackCompleted = true;
+
+        if(this.player.xp) {
+          this.player.xp = this.player.xp + this.monster.xp;
+        } else {
+          this.player.xp = this.monster.xp;
+        }
+        
+        this.logEvent(true, null, null, true);
 
         setTimeout(function(){
-          if (confirm('you won! start new game?')) {
-            self.gameStats.wins++;
-            if (self.gameStats.wins > 2) self.playerHealthCount = self.playerMaxHealth;
-            self.fetchMonsterData();
-          } else {
-            self.gameState = false;
+          self.gameStats.wins++;
+
+          if (self.gameStats.wins % 3 == 0) {
+            self.heal();
           }
-        }, 500);
+        }, 1000);
+      } else if (!this.player.isAlive) {
+        this.gameState = false;
+        this.player.healthCount = 0;
+        this.player.healthBar = 0;
 
-        return true;
-      } else if (this.playerHealthCount && this.playerHealthBar <= 0) {
-        this.playerHealthCount = 0;
-        this.playerHealthBar = 0;
+        this.logEvent(false, null, null, false);
 
-        setTimeout(function(){
-          if (confirm('you lost! start new game?')) {
-            self.gameStats.loses++;
-            self.gameStats.wins = 0;
-            self.startGame();
-          } else {
-            self.gameState = false;
-          }
-        }, 500);
-
-        return true;
+        setTimeout(function() {
+          self.gameStats.loses++;
+          self.gameStats.wins = 0;
+        }, 1000);
       }
-      return false;
     },
 
     fetchPlayerData() {
-      const player = 'http://localhost:3001/api/player';
+      if (!localStorage.getItem('player')) {
+        const playerApi = 'http://localhost:3001/api/player';
 
-      fetch(player)
-      .then(res => res.json())
-      .then(data => {
-        this.playerAvatar = data.avatarUrl;
-        this.playerHealthBar = 100;
-        this.playerMaxHealth = data.hitPoints.max;
-        this.playerHealthCount = this.playerMaxHealth;
-        this.playerName = data.name;
-        this.playerDice = data.inventory.weapons[1].definition.damage.diceString;
-        this.playerWeaponName = data.inventory.weapons[1].definition.name;
-      })
-      .catch(err => { throw err });
+        fetch(playerApi)
+        .then(res => res.json())
+        .then(data => {
+          const player = {
+            isAlive: true,
+            avatar: data.avatarUrl,
+            healthBar: 100,
+            maxHealth: data.hitPoints.max,
+            healthCount: this.player.maxHealth,
+            name: data.name,
+            weapons: data.inventory.weapons
+          };
+
+          this.player = player;
+
+          localStorage.setItem('player', JSON.stringify(player));
+        })
+        .catch(err => { throw err });
+      }
     },
 
-    fetchMonsterData() {
-      const monster = 'http://localhost:3001/api/monster';
+    fetchAllMonsters() {
+      const monster = `https://www.dnd5eapi.co/api/monsters/?${this.challenge_rating}`;
 
       fetch(monster)
       .then(res => res.json())
       .then(data => {
-        this.monsterName = data.name;
-        this.monsterHealth = 100;
-        this.monsterMaxHealth = data.hit_points;
-        this.monsterDice = data.hit_dice;
-
-        if (data.image) this.monsterAvatar = `https://www.dnd5eapi.co${data.image}`;
+        const monsters = data.results;
+        localStorage.setItem('monsters', JSON.stringify(monsters));
       })
       .catch(err => { throw err });
     },
 
-    logEvent: function(isPlayer, character, monster, damage) {
+    fightNewMonster: function() {
+      this.gameState = true;
+      this.monster.attackCompleted = true;
+      this.getMonster();
+    },
+
+    getMonster() {
+      const monsters = JSON.parse(localStorage.getItem('monsters'));
+      const monster = monsters[Math.floor(Math.random() * monsters.length - 1)];
+
+      if (typeof monster === 'object') {
+        fetch(`http://localhost:3001/api/monster/?monster=${monster.index}`)
+        .then(res => res.json())
+        .then(data => {
+          this.monster = {
+            avatar: data.image ? `https://www.dnd5eapi.co${data.image}` : null,
+            attackCompleted: true,
+            isAlive: true,
+            name: data.name,
+            healthBar: 100,
+            health: data.hit_points,
+            dice: data.hit_dice,
+            xp: data.xp
+          }
+        })
+        .catch(err => { throw err }); 
+      } else {
+        this.getMonster();
+      }
+    },
+
+    getPlayer() {
+      if (!localStorage.getItem('player')) {
+        this.fetchPlayerData();
+      } else {
+        const playerData = JSON.parse(localStorage.getItem('player'));
+
+        this.player = {
+          isAlive: true,
+          avatar: playerData.avatar,
+          healthBar: playerData.healthBar,
+          healthCount: playerData.maxHealth,
+          maxHealth: playerData.maxHealth,
+          name: playerData.name,
+          weapons: playerData.weapons
+        };
+      }
+    },
+
+    logEvent: function(isPlayer, damage, weaponName, win) {
       let text;
 
-      if (isPlayer) {
-        text = character + ' attacks ' + monster + ' with ' + this.playerWeaponName + ' for ' + damage + 'hp';
-      } else {
-        text = monster + ' attacks ' + character + ' for ' + damage + 'hp'
+      if (isPlayer && this.player.isAlive && weaponName) {
+        text = `${this.player.name} attacks ${this.monster.name} with ${weaponName} for ${damage}hp`;
+      } else if (this.player.isAlive) {
+        text = `${this.monster.name} attacks ${this.player.name} for ${damage} hp`
+      }
+
+      if (isPlayer && this.player.isAlive && win) {
+        text = `${this.player.name} wins!`;
+      } else if (!isPlayer && !this.player.isAlive && !win)  {
+        text = `${this.monster.name} wins!`;
       }
 
       this.turns.unshift({
@@ -140,85 +194,79 @@ new Vue({
       });
     },
 
-    heal: function(runningAway) {
-      if (runningAway) return;
+    heal: function() {
+      if (this.monster.attackCompleted && this.player.healthCount !== this.player.maxHealth) {
+        let randomHealthCalc = Math.max(Math.floor(Math.random() * 10) + 1, 1);
 
-      if (this.monsterAttackCompleted) {
-        var randomHealthCalc = Math.max(Math.floor(Math.random() * 10) + 1, 1)
-
-        if(this.playerHealthCount <= (this.playerMaxHealth - 10)){
-          this.playerHealthBar += randomHealthCalc;
-          this.playerHealthCount += randomHealthCalc;
+        if(this.player.healthCount <= (this.player.maxHealth - 10)){
+          this.player.healthBar += randomHealthCalc;
+          this.player.healthCount += randomHealthCalc;
         } else {
-          this.playerHealthBar = 100;
-          this.playerHealthCount = this.playerMaxHealth;
+          this.player.healthBar = 100;
+          this.player.healthCount = this.player.maxHealth;
         }
+
+        if (this.monster.isAlive && !this.monster.attackCompleted) this.monsterAttack();
         
         this.turns.unshift({
           isPlayer: true,
-          text: 'player heals for ' + randomHealthCalc
+          text: `${this.player.name} heals for ${randomHealthCalc}`
         });
-
-        if (this.checkWin()) return;
-
-        this.monsterAttack();
       }
     },
 
     monsterAttack: function() {
       const self = this;
-      const damage = this.calcDamage(this.monsterDice);
+      const damage = this.calcDamage(this.monster.dice);
 
-      this.monsterAttackCompleted = false;
+      setTimeout(function() {
+        self.player.healthBar -= Math.floor((damage/self.player.healthCount) * 100);
+        self.player.healthCount -= damage;
+        self.monster.attackCompleted = true;
 
-      setTimeout(function(){
-        self.playerHealthBar -= Math.floor((damage/self.playerHealthCount) * 100);
-        self.playerHealthCount -= damage;
+        self.logEvent(false, damage, null);
+
+        if (self.player.healthCount <= 0) self.player.isAlive = false;
+
         self.checkWin();
-        self.logEvent(false, self.playerName, self.monsterName, damage);
-
-        self.monsterAttackCompleted = true;
       }, 1000);
+    },
+
+    rest: function() {
+      this.player.healthBar = 100;
+      this.player.healthCount = this.player.maxHealth;
     },
 
     runAway: function() {
       var chanceToRunaway = Math.random() < 0.4;
 
       if (chanceToRunaway) {
-        this.fetchMonsterData();
-        this.heal(true);
+        this.getMonster();
+        this.heal();
         this.gameStats.runAways++;
 
         this.turns.unshift({
           isPlayer: true,
-          text: this.playerName + ' succesfully runs away!'
+          text: this.player.name + ' succesfully runs away!'
         });
+
+        return;
       } else {
         this.monsterAttack();
-      }
-    },
 
-    specialAttack: function() {
-      if (this.monsterAttackCompleted) {
-        const damage = this.calcDamage(this.playerDice);
-
-        this.monsterHealth -= Math.floor((damage/this.monsterMaxHealth) * 100);
-        this.monsterMaxHealth -= damage;
-        this.logEvent(true, this.playerName, damage);
-
-        if (this.checkWin()) {
-          return;
-        }
-
-        this.monsterAttack();
+        this.turns.unshift({
+          isPlayer: true,
+          text: this.player.name + ' failed to run away!'
+        });
       }
     },
 
     startGame() {
       this.gameState = true;
       this.turns = [];
-      this.fetchPlayerData();
-      this.fetchMonsterData();
+      this.fetchAllMonsters();
+      this.getMonster();
+      this.getPlayer();
     }
   }
 });
--- app/server/api.js
+++ app/server/api.js
@@ -1,30 +1,29 @@
-var express = require('express');
+const express = require('express');
+const superagent = require('superagent');
 var api = express.Router();
-var request = require('request');
 
 // middleware that is specific to this router
 api.use(function timeLog (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next()
-})
+});
 
 // player data
 api.get('/player', function (req, res) {
   const playerData = require('./json/player');
 
   res.json(playerData.character);
-})
+});
 
 // monster data
-api.get('/monster', function (req, res) {
-  const monsterData = require('./json/monster-list');
-  console.log(monsterData)
-
-  const monster = monsterData.results[Math.floor(Math.random() * monsterData.results.length - 1)];
-
-  request(monster.url).pipe(res);
-})
-
+api.get('/monster/:monster?', function (req, res) {
+  superagent
+    .get(`https://www.dnd5eapi.co/api/monsters/${req.query.monster}`)
+    .then(response => {
+      res.json(response.body);
+    })
+    .catch(console.error);
+});
 
 module.exports = api
